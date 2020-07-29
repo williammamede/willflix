37568c86913ff03ea6470434dbc53c9f494b8d4a
William Mamede <williamsm@inatel.br>
2020-07-29 19:28:24 -0300

    Fix carousel behavior
    Fix carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
+import Slider, { SliderItem } from './components/Slyder';
 
 function Carousel({
   ignoreFirstVideo,
