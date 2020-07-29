efd756ff030b90b223e0906572e6df6712ac2e2b
William Mamede <williamsm@inatel.br>
2020-07-29 19:36:00 -0300

    Add category form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -5,9 +5,23 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 function CadastroCategoria() {
   return (
     <PageDedault>
-        <h1>Cadastro de Video</h1>
-        <Link to="/">
-            Ir para home
+      <h1>Cadastro de categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input type="text"  />
+        </label>
+
+        <button>
+          Cadastrar
+          </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
         </Link>
     </PageDedault>
   )
